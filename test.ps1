8b94ab7217d9c171ffff4d1cd96bdd3343242817
Scott Holden <scott@nullops.io>
2017-06-16 16:03:09 +1000

    Updated test script logic.

--- test.ps1
+++ test.ps1
@@ -1,26 +1,53 @@
 $codeCoverageFile = "CodeCoverage.runsettings"
-$coverageXmlFile = "coverage.coveragexml"
+$coverageXmlFile = "TestResults\coverage.coveragexml"
+$testResultXmlFile = "TestResults\testresults.trx"
 
 $xunitRunnerVSPath = (Resolve-Path ".\packages\xunit.runner.visualstudio.*\build\_common\").Path
 $coverallsPath = (Resolve-Path ".\packages\coveralls.net.*\tools\csmacnz.coveralls.exe").Path
 
 $unitTestsPath = ".\tests\Serilog.Sinks.AzureWebJobsTraceWriter.UnitTests\bin\Release\Serilog.Sinks.AzureWebJobsTraceWriter.UnitTests.dll"
 
-$vsCodeCoverageExe = "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe"
+$vsCodeCoverageExe = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe"
 
-vstest.console.exe /inIsolation /Enablecodecoverage /TestAdapterPath:"$xunitRunnerVSPath" "$unitTestsPath" /Settings:$codeCoverageFile
+if (!(Test-Path -Path $vsCodeCoverageExe))
+{
+    $vsCodeCoverageExe = "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe"
+}
 
-$coverageFilePath = (Resolve-Path -path "TestResults\*\*.coverage").Path
+# Cleanup before we start
 
+if (Test-Path -Path "TestResults\")
+{
+     Remove-Item -Path "TestResults\" -Recurse -Force
+}
+if (Test-Path -Path ".\$coverageXmlFile")
+{
+     Remove-Item -Path ".\$coverageXmlFile"
+}
 if (Test-Path -Path ".\$coverageXmlFile")
 {
      Remove-Item -Path ".\$coverageXmlFile"
 }
 
-Push-AppveyorArtifact "$coverageFilePath"
+vstest.console.exe /inIsolation /Enablecodecoverage /TestAdapterPath:"$xunitRunnerVSPath" /Settings:$codeCoverageFile /logger:trx "$unitTestsPath" 
 
-.$vsCodeCoverageExe analyze /output:$coverageXmlFile "$coverageFilePath"
+$coverageFilePath = (Resolve-Path -path "TestResults\*\*.coverage").Path
+$trxSourcePath = @(Resolve-Path -path "TestResults\*.trx")[0].Path
 
-Push-AppveyorArtifact "$coverageXmlFile"
+if (Test-Path -Path $coverageXmlFile)
+{
+     Remove-Item -Path $coverageXmlFile
+}
+if (Test-Path -Path $testResultXmlFile)
+{
+     Remove-Item -Path $testResultXmlFile
+}
+
+Copy-Item -Path $trxSourcePath -Destination $testResultXmlFile
+
+& $vsCodeCoverageExe analyze /output:$coverageXmlFile "$coverageFilePath"
+
+Push-AppveyorArtifact $testResultXmlFile
+Push-AppveyorArtifact $coverageXmlFile
 
 & $coverallsPath --dynamiccodecoverage -i $coverageXmlFile --repoToken $env:COVERALLS_REPO_TOKEN --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB_ID --useRelativePaths
