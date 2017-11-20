cede50a58f15e6bcc03a93fa0e7944e92ec8a162
Vlad Kalinin <vkalinin@starrez.com>
2017-11-20 14:54:09 +1100

    Converted library to .NET Standard and updated references

--- src/Serilog.Sinks.AzureWebJobsTraceWriter/Properties/AssemblyInfo.cs
+++ src/Serilog.Sinks.AzureWebJobsTraceWriter/Properties/AssemblyInfo.cs
@@ -1,37 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Serilog.Sinks.AzureWebJobsTraceWriter")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Serilog.Sinks.AzureWebJobsTraceWriter")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b67a68ad-36d3-47ab-9846-a21eceada700")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: InternalsVisibleTo("Serilog.Sinks.AzureWebJobsTraceWriter.UnitTests")]
